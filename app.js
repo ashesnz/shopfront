6590b5fe0d3e5a81623c9fb1fd836a773708cb9f
Jim <jburg79@gmail.com>
2012-10-07 07:54:46 -0400

    adding bootstrap files

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var routes = require('./routes')
+var user = require('./routes/user')
+var http = require('http')
+var path = require('path');
 
 var app = express();
 
